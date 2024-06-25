9c6c5cfb5d255e65c292697a759a6373b6e9775b
vinsin2002 <vinsin2002@gmail.com>
2024-06-25 14:40:37 +0530

    d codes

--- GraphDSL/graphcode/wiki_bench/metis/csr_conversion.h
+++
@@ -1,9 +0,0 @@
-// csr_conversion.h
-
-#include <vector>
-#include <unordered_map>
-
-void convertToCSR(const std::vector<std::unordered_map<int, int>>& graph, 
-                  std::vector<int>& xadj, 
-                  std::vector<int>& adjncy,
-                  std::vector<int>& weights);
--- GraphDSL/graphcode/wiki_bench/metis/partitioned_writer.h
+++
@@ -1,8 +0,0 @@
-// partitioned_writer.h
-
-#include <vector>
-#include <string>
-
-void writePartitionedGraph(const std::vector<std::tuple<int, int, int>>& edges, 
-                           const std::vector<int>& partitionResult, 
-                           int numPartitions, int offset, std::string outputFolderPath);
