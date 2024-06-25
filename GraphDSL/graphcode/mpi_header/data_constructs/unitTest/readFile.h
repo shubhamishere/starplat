4ce9356e46dc6ad937290e4130e12a5143e1e5e9
vinsin2002 <vinsin2002@gmail.com>
2024-06-25 14:08:27 +0530

    removed whole graphdsl copy

--- GraphDSL/graphcode/mpi_header/data_constructs/unitTest/readFile.h
+++
@@ -1,7 +0,0 @@
-#ifndef __FILE_READER__
-#define __FILE_READER__
-#include <vector>
-#include <stdio.h>
-void readFile (char * fileName, int &size, std::vector<std::vector<int> > &transactions) ;
-void writeFile (char * fileName, std::vector<int> result ) ;
-#endif
