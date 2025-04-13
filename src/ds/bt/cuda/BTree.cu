fd112aabb921de70d4d39b44ec1b9ee9fd8cbf82
PRITAM DE <pritamde090920@gmail.com>
2025-04-13 18:45:29 +0530

    Added key-value pair support for the  Btree data structure

--- src/ds/bt/cuda/BTree.cu
+++ src/ds/bt/cuda/BTree.cu
@@ -1,7 +1,9 @@
 // including the header file
-#include "BTree.h"
+#include "btree.h"
 
 // The different parameters for the code
+#define MAX_SIZE 32
+#define SEARCH_BLOCK_SIZE 64
 #define BLOCK_SIZE 1024
 #define SEARCH_INNER_KERNEL 2
 #define INSERTION_KERNEL 2
@@ -12,10 +14,10 @@
 
 
 // kernel to perform parallel insertion
-__global__ void insertion(int *btree, int **holes, int *elementCounter, int *batchElements, int *insertionChunks, unsigned int *lastHoleCounter, int addCount){
+__global__ void insertion(Node *btreeArray, int **holes, int *elementCounter, Node *batchElements, int *insertionChunks, unsigned int *lastHoleCounter, int addCount){
     /*
         Parameters:
-            btree : pointer to btree array
+            btreeArray : pointer to btree array
             holes : pointer to 2D holes array
             elementCounter : pointer to an array that stores the current total number of elements in each chunk
             batchElements : pointer to the batch to insert
@@ -33,7 +35,9 @@ __global__ void insertion(int *btree, int **holes, int *elementCounter, int *bat
         if ((tid * INSERTION_KERNEL + i) >= addCount) break;
 
         // calculating the cluster where the element should be inserted
-        int elem = batchElements[tid * INSERTION_KERNEL + i];
+        int elem = batchElements[tid * INSERTION_KERNEL + i].key;
+        int elemValue = batchElements[tid * INSERTION_KERNEL + i].value;
+
         int cluster = abs((elem + MAX_VALUE_RANGE) / (4 * CHUNK_SIZE));
 
         // a variable to be used to acquire lock on the holes array
@@ -56,7 +60,9 @@ __global__ void insertion(int *btree, int **holes, int *elementCounter, int *bat
                 //acquiring lock and inserting the element
                 int oldVal = atomicCAS(&holes[cluster][index], hole, INT_MIN);
                 if(oldVal == hole){
-                    btree[hole] = elem;
+                    // inserting the key and the value into the btree node
+                    btreeArray[hole].key = elem;
+                    btreeArray[hole].value = elemValue;
                     elementInserted = true;
                     break;
                 }
@@ -76,10 +82,12 @@ __global__ void insertion(int *btree, int **holes, int *elementCounter, int *bat
                 continue;
             }
 
-            // insert the element to the btree by calculating the correct index
+            // icalculating the correct index where the element should be inserted
             int insertChunkIndex = atomicAdd(&elementCounter[chunkIndex], 1);
             int insertionIndex = cluster * 4 * CHUNK_SIZE + chunk * CHUNK_SIZE + insertChunkIndex;
-            btree[insertionIndex] = elem;
+            // inserting the key and the value into the btree node
+            btreeArray[insertionIndex].key = elem;
+            btreeArray[insertionIndex].value = elemValue;
             elementInserted = true;
         }
     }
@@ -87,10 +95,10 @@ __global__ void insertion(int *btree, int **holes, int *elementCounter, int *bat
 
 
 // child kernel to perform parallel searching
-__global__ void searching(int *btree, int *batchElements, int *searchResult, int searchResultIndex, int searchCount) {
+__global__ void parallelSearching(Node *btreeArray, int *batchElements, int *searchResult, int searchResultIndex, int searchCount) {
     /*
         Parameters:
-            btree : pointer to btree array
+            btreeArray : pointer to btree array
             batchElements : pointer to the batch to search
             searchResult : pointer to an array that stored the result of searching
             searchResultIndex : index of the batch for which the child kernel was launched
@@ -109,19 +117,19 @@ __global__ void searching(int *btree, int *batchElements, int *searchResult, int
 
         int clusterIndex = abs((num + MAX_VALUE_RANGE) / (4 * CHUNK_SIZE));
 
-        // if the element is found in the btree then add that index into result array
-        if(btree[clusterIndex * 4 * CHUNK_SIZE + tid] == num) {
-            searchResult[searchResultIndex * SEARCH_INNER_KERNEL + i] = clusterIndex * 4 * CHUNK_SIZE + tid;
+        // if the key of the element is found in the btree then add the value corresponding to the key into result array
+        if(btreeArray[clusterIndex * 4 * CHUNK_SIZE + tid].key == num) {
+            searchResult[searchResultIndex * SEARCH_INNER_KERNEL + i] = btreeArray[clusterIndex * 4 * CHUNK_SIZE + tid].value;
         }
     }
 }
 
 
 // parent kernel to perform parallel searching
-__global__ void batchSearch(int *btree, int *batchElements, int *searchResult, int searchCount) {
+__global__ void searching(Node *btreeArray, int *batchElements, int *searchResult, int searchCount) {
     /*
         Parameters:
-            btree : pointer to btree array
+            btreeArray : pointer to btree array
             batchElements : pointer to the batch to search
             searchResult : pointer to an array that stored the result of searching
             searchCount : an integer denoting the number of elements in the current batch to be searched
@@ -138,17 +146,48 @@ __global__ void batchSearch(int *btree, int *batchElements, int *searchResult, i
 
     // launching the child kernel
     dim3 blocks = dim3(ceil(4 * CHUNK_SIZE / (double)BLOCK_SIZE), 1, 1);
-    searching<<<blocks, BLOCK_SIZE>>> (btree, batchElements, searchResult, tid, searchCount);
+    parallelSearching<<<blocks, BLOCK_SIZE>>> (btreeArray, batchElements, searchResult, tid, searchCount);
+}
+
+
+// child kernel to perform searching and populating the during deletion
+__global__ void deletionSearching(Node *btreeArray, int *batchElements, int searchResultIndex, int searchCount) {
+    /*
+        Parameters:
+            btreeArray : pointer to btree array
+            batchElements : pointer to the batch to search
+            searchResult : pointer to an array that stored the result of searching
+            searchResultIndex : index of the batch for which the child kernel was launched
+            searchCount : an integer denoting the number of elements in the current batch to be searched
+    */
+
+    // calculating the thread id
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+
+    // running a loop over the number of elements each thread is supposed to search
+    for (int i = 0; i < SEARCH_INNER_KERNEL; i++) {
+        // adding out of bounds check
+        if ((searchResultIndex * SEARCH_INNER_KERNEL + i) >= searchCount) break;
+
+        int num = batchElements[searchResultIndex * SEARCH_INNER_KERNEL + i];
+
+        int clusterIndex = abs((num + MAX_VALUE_RANGE) / (4 * CHUNK_SIZE));
+
+        // if the element is found in the btree then mark that index as deleted (replace by INT_MIN)
+        if(btreeArray[clusterIndex * 4 * CHUNK_SIZE + tid].key == num) {
+            btreeArray[clusterIndex * 4 * CHUNK_SIZE + tid].key = INT_MIN;
+            btreeArray[clusterIndex * 4 * CHUNK_SIZE + tid].value = INT_MIN;
+        }
+    }
 }
 
 
 // parent kernel to perform parallel deleting
-__global__ void deletion(int *btree, int *batchElements , int *deletionIndex, int **holes, unsigned int *lastHoleCounter, int deleteCount){
+__global__ void deletion(Node *btreeArray, int *batchElements, int **holes, unsigned int *lastHoleCounter, int deleteCount){
      /*
         Parameters:
-            btree : pointer to btree array
+            btreeArray : pointer to btree array
             batchElements : pointer to the batch to delete
-            deletionIndex : pointer to array that stores the index in the btree where the element is located (so that deletion can be done there)
             holes : pointer to 2D holes array
             lastHoleCounter : pointer to an array that stores the number of elements present in the holes array for the current cluster
             deleteCount : an integer denoting the number of elements in the current batch to be deleted
@@ -157,48 +196,34 @@ __global__ void deletion(int *btree, int *batchElements , int *deletionIndex, in
     // calculating the thread id
     int tid = blockDim.x * blockIdx.x + threadIdx.x; 
 
-    int num = batchElements[tid];
-
-    // setting the deletion index to INT_MIN (that will indicate that the element to be deleted does not exist in the btree)
-    deletionIndex[tid] = INT_MIN;
-
-    int clusterIndex = abs((num + MAX_VALUE_RANGE) / (4 * CHUNK_SIZE));
+    // adding out of bounds check
+    if (tid >= deleteCount) return;
 
     // launching the search kernel to search for the element
-    dim3 blocks=dim3(ceil(4 * CHUNK_SIZE / (double) BLOCK_SIZE), 1, 1);
-    searching<<<blocks, BLOCK_SIZE>>> (btree, batchElements, deletionIndex, tid, deleteCount);
-
-    // if the element actually exists in the btree then delete it
-    if(deletionIndex[tid] >= 0) {
-        // mark the index in the holes array
-        int holeIndex = atomicInc(&lastHoleCounter[clusterIndex], MAX_HOLES_SIZE);
-        holes[clusterIndex][holeIndex] = deletionIndex[tid];
-
-        // make that index in the btree as INT_MIN that will indicate that the element no longer exists in the array (this is helpful for searching)
-        btree[deletionIndex[tid]] = INT_MIN;
-    }
+    dim3 blocks = dim3(ceil(4 * CHUNK_SIZE / (double) BLOCK_SIZE), 1, 1);
+    deletionSearching<<<blocks, BLOCK_SIZE>>> (btreeArray, batchElements, tid, deleteCount);
 }
 
 
 // constructor
-BTree::BTree(int MAX_SIZE) {
+btree::btree() {
     // calling the function to allocate the memory for the different arrays
-    initMemory(MAX_SIZE);
+    initMemory();
 }
 
 
 // destructor
-BTree::~BTree() {
+btree::~btree() {
     // calling the function to deallocate the memory for the different arrays
     freeMemory();
 }
 
 
 // function to allocate memory for the different arrays
-void BTree::initMemory(int MAX_SIZE) {
-    // allocate btree on device and initialoze all indices to 0
-    cudaMalloc(&btree, MAX_BTREE_SIZE * sizeof(int));
-    cudaMemset(btree, 0, MAX_BTREE_SIZE * sizeof(int));
+void btree::initMemory() {
+    // allocate btree on device and initialize all indices to 0
+    cudaMalloc(&btreeArray, MAX_BTREE_SIZE * sizeof(Node));
+    cudaMemset(btreeArray, 0, MAX_BTREE_SIZE * sizeof(Node));
 
     // allocate holes array on device
     int numClusters = MAX_BTREE_SIZE / (4 * CHUNK_SIZE);
@@ -217,23 +242,36 @@ void BTree::initMemory(int MAX_SIZE) {
     // host side initialization
     insertionChunkPos = 0;
     insertionChunksHost = (int*)malloc(MAX_SIZE * sizeof(int));
+
+    // allocating memory for the batch arrays
+    addBatch = (Node*)malloc(MAX_SIZE * sizeof(Node));
+    searchBatch = (int*)malloc(SEARCH_BLOCK_SIZE * sizeof(int));
+    deleteBatch = (int*)malloc(MAX_SIZE * sizeof(int));
+
+    // initializing the count of elements in the batches
+    addCount = 0;
+    searchCount = 0;
+    deleteCount = 0;
 }
 
 
 // function to deallocate memory for the different arrays
-void BTree::freeMemory() {
+void btree::freeMemory() {
     // device deallocations
-    cudaFree(btree);
+    cudaFree(btreeArray);
     cudaFree(holes);
     cudaFree(elementCounter);
 
     // host deallocations
     free(insertionChunksHost);
+    free(addBatch);
+    free(searchBatch);
+    free(deleteBatch);
 }
 
 
-// insert function for user
-void BTree::insert(int *addBatch, int addCount) {
+// insert function for user to insert a batch
+void btree::batchInsert(Node *addBatch, int addCount) {
     /*
         Parameters:
             addBatch : pointer to the batch to add
@@ -251,27 +289,28 @@ void BTree::insert(int *addBatch, int addCount) {
     cudaMalloc(&insertionChunks, addCount * sizeof(int));
     cudaMemcpy(insertionChunks, insertionChunksHost, addCount * sizeof(int), cudaMemcpyHostToDevice);
 
-    int *batchElements;
-    cudaMalloc(&batchElements, addCount * sizeof(int));
-    cudaMemcpy(batchElements, addBatch, addCount * sizeof(int), cudaMemcpyHostToDevice);
+    Node *batchElements;
+    cudaMalloc(&batchElements, addCount * sizeof(Node));
+    cudaMemcpy(batchElements, addBatch, addCount * sizeof(Node), cudaMemcpyHostToDevice);
 
     // launching the kernel
     dim3 threads = addCount <= 1024 ? dim3(addCount, 1, 1) : dim3(1024, 1, 1);
     dim3 blocks = dim3(ceil(addCount / (double) BLOCK_SIZE), 1, 1);
 
-    insertion<<<blocks,threads>>> (btree, holes, elementCounter, batchElements, insertionChunks, lastHoleCounter, addCount);
+    insertion<<<blocks,threads>>> (btreeArray, holes, elementCounter, batchElements, insertionChunks, lastHoleCounter, addCount);
     cudaDeviceSynchronize();
 
     // freeing the temporary device arrays
     cudaFree(batchElements);
     cudaFree(insertionChunks);
 
+    // resetting the random chunk array count
     insertionChunkPos = 0;
 }
 
 
-// search function for user
-int* BTree::search(int *searchBatch, int searchCount) {
+// search function for user to search a batch on the host end
+int* btree::batchSearchHost(int *searchBatch, int searchCount) {
     /*
         Parameters:
             searchBatch : pointer to the batch to search
@@ -289,25 +328,50 @@ int* BTree::search(int *searchBatch, int searchCount) {
     // launching the kernel
     int threads = ceil((double)searchCount / SEARCH_INNER_KERNEL);
     
-    batchSearch<<<1, threads>>> (btree, batchElements, searchResult, searchCount);                        
+    searching<<<1, threads>>> (btreeArray, batchElements, searchResult, searchCount);                        
     
     // copying back the results from the device to host
     int *searchResultHost = (int*)malloc(searchCount * sizeof(int));
     
     cudaMemcpy(searchResultHost, searchResult, searchCount * sizeof(int), cudaMemcpyDeviceToHost);
-    cudaDeviceSynchronize();
     
     // freeing the temporary device arrays
     cudaFree(batchElements);
     cudaFree(searchResult);
-    free(searchResultHost);
 
+    // returning the result of the search back to the caller function
     return searchResultHost;
 }
 
 
-// delete function for user
-void BTree::remove(int *deleteBatch, int deleteCount) {
+// search function for user to search a batch on the device end
+int* btree::batchSearchDevice(int *batchElements, int searchCount) {
+    /*
+        Parameters:
+            batchElements : pointer to the batch to search
+            searchCount : an integer denoting the number of elements in the current batch to be searched
+    */
+
+    // allocating memory on the device and copying the contents
+    int *searchResult;
+    cudaMalloc(&searchResult, searchCount * sizeof(int));
+
+    // launching the kernel
+    int threads = ceil((double)searchCount / SEARCH_INNER_KERNEL);
+    
+    searching<<<1, threads>>> (btreeArray, batchElements, searchResult, searchCount);                        
+    cudaDeviceSynchronize();
+    
+    // freeing the temporary device arrays
+    cudaFree(batchElements);
+
+    // returning the result of the search back to the caller function
+    return searchResult;
+}
+
+
+// delete function for user to delete a batch
+void btree::batchRemove(int *deleteBatch, int deleteCount) {
     /*
         Parameters:
             deleteBatch : pointer to the batch to delete
@@ -315,20 +379,97 @@ void BTree::remove(int *deleteBatch, int deleteCount) {
     */
 
     // allocating memory on the device and copying the contents
-    int *deletionIndex;
-    cudaMalloc(&deletionIndex, deleteCount * sizeof(int));
-
     int *batchElements;
     cudaMalloc(&batchElements, deleteCount * sizeof(int));
     cudaMemcpy(batchElements, deleteBatch, deleteCount * sizeof(int), cudaMemcpyHostToDevice);
     
     // launching the kernel
-    dim3 blocks = dim3(ceil(4 * CHUNK_SIZE / (double)BLOCK_SIZE), 1, 1);
+    int threads = ceil((double)deleteCount / BLOCK_SIZE);
 
-    deletion<<<1, deleteCount>>> (btree, batchElements, deletionIndex, holes, lastHoleCounter, deleteCount);
+    deletion<<<1, threads>>> (btreeArray, batchElements, holes, lastHoleCounter, deleteCount);
     cudaDeviceSynchronize();
 
     // freeing the temporary device arrays
     cudaFree(batchElements);
-    cudaFree(deletionIndex);
+}
+
+
+// declaring a host device function to insert one key-value pair
+__host__ __device__ void btree::insertNode(int key, int value) {
+    /*
+        Parameters:
+            key : key of the element which is to be inserted
+            value : value corresponding to the key of the element which is to be inserted
+    */
+
+    // creating the node object
+    Node newNode(key, value);
+
+    // adding the element to the batch
+    addBatch[addCount++] = newNode;
+
+    // calling the batch insert function to insert the element (the size of the batch passed is of one element)
+    batchInsert(addBatch, addCount);
+
+    // resetting the batch size count
+    addCount = 0;
+}
+
+
+// declaring a host function to search one key-value pair
+__host__ int* btree::searchHost(int key) {
+    /*
+        Parameters:
+            key : key of the element which is to be searched
+    */
+
+    // adding the key to the batch
+    searchBatch[searchCount++] = key;
+
+    // calling the batch search function to search for the key (the size of the batch passed is of one element)
+    int* searchResult = batchSearchHost(searchBatch, searchCount);
+
+    // resetting the batch size count
+    searchCount = 0;
+
+    // returing back the pointer to the array that stores the result of the search
+    return searchResult;
+}
+
+
+// declaring a host function to search one key-value pair
+__device__ int* btree::searchDevice(int key) {
+    /*
+        Parameters:
+            key : key of the element which is to be searched
+    */
+
+    // adding the key to the batch
+    int *batchElements;
+    cudaMalloc(&batchElements, sizeof(int));
+    batchElements[0] = key;
+
+    // calling the batch search function to search for the key (the size of the batch passed is of one element)
+    int* searchResult = batchSearchDevice(batchElements, 1);
+
+    // returing back the pointer to the array that stores the result of the 
+    return searchResult;
+}
+
+
+// declaring a host device function to delete one key-value pair
+__host__ __device__ void btree::remove(int key) {
+    /*
+        Parameters:
+            key : key of the element which is to be deleted
+    */
+
+    // adding the key to the batch
+    deleteBatch[deleteCount++] = key;
+
+    // calling the batch search function to search for the key (the size of the batch passed is of one element)
+    batchRemove(deleteBatch, deleteCount);
+
+    // resetting the batch size count
+    deleteCount = 0;
 }
